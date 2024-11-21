9ee0a9abebb4fa5cc955298516130de89d7efee7
bluezdot <72647326+bluezdot@users.noreply.github.com>
2024-11-21 18:33:43 +0700

    feat: mesh service idea on server

---
+++ src/test/send/mesh-handler.ts
@@ -0,0 +1,85 @@
+import {BlockfrostProvider, MeshTxBuilder, MeshWallet, mnemonicToEntropy, UtxoSelection} from '@meshsdk/core';
+import {csl} from "@meshsdk/core-csl";
+import * as CardanoWasm from "@emurgo/cardano-serialization-lib-nodejs";
+
+const SEND_TOKEN = 'lovelace'
+const SEND_AMOUNT = '150000000';
+const RECEIVER = 'addr_test1qzkjkwkyuvqh4hanewcych985euzqnd24jt2ej4kdyqpphr6hx9nax27yydcv9djgekn4ylvq60f6c830ng77dp5af2qrphdqh';
+const SENDER = 'addr_test1qr3nhq5hu88xjls8kyk790rcz2qt43aee9yuvx2gjn5msgfjwcw036s7adnz3f8ufl85guxkhz3cvv4znrvy7rzmv0qquff93j';
+const API_KEY = 'preprodcnP5RADcrWMlf2cQe4ZKm4cjRvrBQFXM';
+
+function harden(num: number): number {
+    return 0x80000000 + num;
+}
+
+async function main() {
+    // 1. Keyring
+    const mnemonic = 'north until topple blame bracket potato hawk repeat pyramid mutual question barely unfair fox office history famous marriage acid undo useful sail play trend';
+    const entropy = mnemonicToEntropy(mnemonic);
+    const rootKey = CardanoWasm.Bip32PrivateKey.from_bip39_entropy(
+        Buffer.from(entropy, 'hex'),
+        Buffer.from(''),
+    );
+    const accountKey = rootKey
+        .derive(harden(1852))
+        .derive(harden(1815))
+        .derive(harden(0));
+    const paymentPrvKey = accountKey
+        .derive(0)
+        .derive(0).to_raw_key();
+    const paymentPubKey = accountKey
+        .derive(0)
+        .derive(0)
+        .to_public();
+    const stakeKey = accountKey
+        .derive(2) // chimeric
+        .derive(0)
+        .to_public();
+    const baseAddress = CardanoWasm.BaseAddress.new(
+        CardanoWasm.NetworkInfo.testnet_preprod().network_id(),
+        CardanoWasm.Credential.from_keyhash(paymentPubKey.to_raw_key().hash()),
+        CardanoWasm.Credential.from_keyhash(stakeKey.to_raw_key().hash()),
+    );
+    const address = baseAddress.to_address().to_bech32();
+
+    // 2. Build tx
+    const blockchainProvider = new BlockfrostProvider(API_KEY);
+    const utxos = await blockchainProvider.fetchAddressUTxOs(SENDER);
+    const txBuilder = new MeshTxBuilder({
+        fetcher: blockchainProvider,
+        evaluator: blockchainProvider
+    });
+    const unsignedTx = await txBuilder
+        .txOut(RECEIVER, [
+            {
+                unit: SEND_TOKEN,
+                quantity: SEND_AMOUNT
+            }
+        ])
+        .changeAddress(SENDER)
+        .selectUtxosFrom(utxos)
+        .complete();
+
+    console.log('unsignedTx', unsignedTx);
+
+    const cslUnsignedTx = CardanoWasm.FixedTransaction.from_hex(unsignedTx);
+    cslUnsignedTx.sign_and_add_vkey_signature(CardanoWasm.PrivateKey.from_hex(paymentPrvKey.to_hex()))
+
+    const signedTx = cslUnsignedTx.to_hex();
+    // console.log('[i] csl', signedTx)
+    // const wallet = new MeshWallet({
+    //     networkId: 0,
+    //     fetcher: blockchainProvider,
+    //     submitter: blockchainProvider,
+    //     key: {
+    //         type: 'mnemonic',
+    //         words: 'north until topple blame bracket potato hawk repeat pyramid mutual question barely unfair fox office history famous marriage acid undo useful sail play trend'.split(' ')
+    //     },
+    // });
+    // console.log('[i] mesh', wallet.signTx(unsignedTx));
+
+    const txHash = await blockchainProvider.submitTx(signedTx);
+    console.log('txHash', txHash);
+}
+
+main().catch((error) => {console.log('error', error)})
--- src/test/send/mesh.ts
+++ src/test/send/mesh.ts
@@ -1,49 +1,64 @@
-import {AppWallet, BlockfrostProvider, MeshTxBuilder, Transaction} from '@meshsdk/core';
-import {UTxO} from "@meshsdk/common";
+import {BlockfrostProvider, MeshTxBuilder, MeshWallet, UtxoSelection} from '@meshsdk/core';
+import {csl} from "@meshsdk/core-csl";
 
-const SEND_AMOUNT = '1500000000';
+const SEND_AMOUNT = '150000000';
 const RECEIVER = 'addr_test1qzkjkwkyuvqh4hanewcych985euzqnd24jt2ej4kdyqpphr6hx9nax27yydcv9djgekn4ylvq60f6c830ng77dp5af2qrphdqh';
-const UTXOS = [{ // todo: create function to fetch lastest utxo and select utxo
-    input: {
-        txHash: '73b0023a1c84a5c50e21110efa39328e73f57e93981a496ab5d74d23325a9aec',
-        outputIndex: 1
-    },
-    output: {
-        address: 'addr_test1qr3nhq5hu88xjls8kyk790rcz2qt43aee9yuvx2gjn5msgfjwcw036s7adnz3f8ufl85guxkhz3cvv4znrvy7rzmv0qquff93j',
-        amount: [{
-            unit: 'lovelace',
-            quantity: "9846492365"
-        }]
-    }
-}] as UTxO[]
+const SENDER = 'addr_test1qr3nhq5hu88xjls8kyk790rcz2qt43aee9yuvx2gjn5msgfjwcw036s7adnz3f8ufl85guxkhz3cvv4znrvy7rzmv0qquff93j';
+const MNEMONIC = 'north until topple blame bracket potato hawk repeat pyramid mutual question barely unfair fox office history famous marriage acid undo useful sail play trend';
+const API_KEY = 'preprodcnP5RADcrWMlf2cQe4ZKm4cjRvrBQFXM';
 
 async function main() {
-    const blockchainProvider = new BlockfrostProvider('preprodcnP5RADcrWMlf2cQe4ZKm4cjRvrBQFXM');
-    const wallet = new AppWallet({
+    const blockchainProvider = new BlockfrostProvider(API_KEY);
+    const wallet = new MeshWallet({
         networkId: 0,
         fetcher: blockchainProvider,
         submitter: blockchainProvider,
         key: {
             type: 'mnemonic',
-            words: 'north until topple blame bracket potato hawk repeat pyramid mutual question barely unfair fox office history famous marriage acid undo useful sail play trend'.split(' ')
+            words: MNEMONIC.split(' ')
         },
     });
+
+    // 1. Use MeshJS
+    // const utxos = await wallet.getUtxos('payment');
+    // 2. Get manually
+    const utxos = await blockchainProvider.fetchAddressUTxOs(SENDER);
 
     const txBuilder = new MeshTxBuilder({
         fetcher: blockchainProvider,
         evaluator: blockchainProvider,
-        verbose: true,
+        verbose: false,
     });
 
     const unsignedTx = await txBuilder
-        .txOut(RECEIVER, [{ unit: 'lovelace', quantity: SEND_AMOUNT }])
-        .changeAddress('addr_test1qr3nhq5hu88xjls8kyk790rcz2qt43aee9yuvx2gjn5msgfjwcw036s7adnz3f8ufl85guxkhz3cvv4znrvy7rzmv0qquff93j')
-        .selectUtxosFrom(UTXOS)
+        .txOut(RECEIVER, [
+            {
+                unit: 'lovelace',
+                quantity: SEND_AMOUNT
+            }
+        ])
+        .changeAddress(SENDER)
+        .selectUtxosFrom(utxos)
         .complete();
 
-    const signedTx = wallet.signTx(unsignedTx);
+    const params = await blockchainProvider.fetchProtocolParameters();
+    const coefficientFee = params.minFeeA;
+    const constantFee = params.minFeeB;
+
+    const txFee = csl.Transaction.from_hex(unsignedTx).body().fee().to_str();
+
+    const signedTx = wallet.signTx(unsignedTx); // cbor
     const txHash = await wallet.submitTx(signedTx);
     console.log('txHash', txHash);
 }
 
 main().catch((error) => {console.log('error', error)})
+
+// interface CardanoTxConfig {
+//     provider: string,
+//     isTestnet: boolean,
+//     policyId: string,
+//     amount: string,
+//     receiver: string,
+//     sender: string
+// }
